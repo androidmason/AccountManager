846611da9de2377c29ec038d3afa185e9b04e617
Monil <androidmason@bitbucket.org>
2016-03-10 10:58:46 +0530

    Added authentication to enter admin module

---
+++ app/controllers/Authentication.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import play.api.mvc.Controller
+import play.api.mvc.Action
+import models.Person
+import play.api.data.Form
+import play.api.data.Forms._
+
+class Authentication extends Controller{
+  
+ def getLogin = Action {
+    Ok(views.html.login("Hello"))
+  }
+ 
+ val loginForm: Form[Person] = Form {
+    mapping(
+      "pass" -> text)(Person.apply)(Person.unapply)
+  }
+  
+ def authenticate = Action { implicit request =>
+   val person = loginForm.bindFromRequest.get
+   if (person.pass.equals("committee@321"))
+     Redirect(routes.Application.index())
+   else
+     Redirect(routes.Authentication.getLogin())
+ }
+ 
+}
