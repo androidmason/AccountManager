8da432142242be24d1d8bacac3821269462bd4af
Monil <androidmason@bitbucket.org>
2016-04-16 21:46:14 +0530

    added ledger view with public access

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,7 @@ import models.{ DB, Person }
 import play.api.db.Databases
 import models.BalanceSheet
 import play.api.libs.json.Json
+import models.Ledger
 
 class Application extends Controller {
 
@@ -18,6 +19,13 @@ class Application extends Controller {
     implicit val jsonFormat = Json.format[BalanceSheet]
     val members = DB.loadData()
     Ok(Json.toJson(members))
+  }
+  
+  def getLedger = Action {
+//    implicit val jsonFormat = Json.format[Ledger]
+//    val ledger = DB.loadLedger
+//    Ok(Json.toJson(ledger))
+    Ok(views.html.ledger("Hello",DB.loadLedger))
   }
 
   def addPerson = Action {
